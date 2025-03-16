504ad16c18f65fd9a647a99a67ae37bd99823e01
João Victor Quintino de Brito <joaovictorquintinobrito@gmail.com>
2025-03-16 15:51:49 -0300

    Inico do Projeto

---
+++ Novo-Imperio/src/main/java/br/com/NovoImperioBarbearia/Novo/Imperio/NovoImperioApplication.java
@@ -0,0 +1,13 @@
+package br.com.NovoImperioBarbearia.Novo.Imperio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoImperioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoImperioApplication.class, args);
+	}
+
+}
---
+++ Novo-Imperio/src/test/java/br/com/NovoImperioBarbearia/Novo/Imperio/NovoImperioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.NovoImperioBarbearia.Novo.Imperio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovoImperioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
