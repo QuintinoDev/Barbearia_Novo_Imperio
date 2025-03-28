62c63479140ecd3d8213adae0e5419d11f9cdf14
João Victor Quintino de Brito <joaovictorquintinobrito@gmail.com>
2025-03-28 15:05:32 -0300

    Fazendo validação e autenticação d o token JWT via cabeçalho da requisição

--- Novo-Imperio/src/main/java/br/com/barbershop/util/security/SecurityConfigurations.java
+++ Novo-Imperio/src/main/java/br/com/barbershop/util/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.barbershop.util.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,21 +11,30 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 //Classe criada para configurar nosso segurança do condigo
 public class SecurityConfigurations {
 
+    @Autowired
+    private SecurityFilter securityFilter;
 
     //Essa anotação serve para exportar uma classe para o spring, fazendo com que ele consiga carrega-la e realize sua injeção
     @Bean
     //SecurityFilterChain objeto do spring usado para configurar essa parte de autenticação e de autorização
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        //Estou desabilitando o csrf porque vamos usar o token que ja proteje desse ataque
-        return http.csrf(c -> c.disable())
-                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return
+                http.csrf(csrf -> csrf.disable())
+                        .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .authorizeHttpRequests(req -> {
+                            req.requestMatchers("/login").permitAll();
+                            req.anyRequest().authenticated();
+                        })
+                        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                        .build();
+
     }
 
     @Bean
--- Novo-Imperio/src/main/java/br/com/barbershop/util/security/TokenService.java
+++ Novo-Imperio/src/main/java/br/com/barbershop/util/security/TokenService.java
@@ -4,6 +4,7 @@ import br.com.barbershop.model.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -36,6 +37,20 @@ public class TokenService {
         }
 
     }
+// Methodo para fazer validação do token que esta sendo gerado
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API barbershop")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
 //  Tempo que nosso token vai ficar tivo apos ser gerado
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
