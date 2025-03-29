20587925a6f3e54ae60271b65280f44de16f336f
João Victor Quintino de Brito <joaovictorquintinobrito@gmail.com>
2025-03-29 14:57:27 -0300

    Tirando espaços desnecessário

--- Novo-Imperio/src/main/java/br/com/barbershop/util/security/SecurityConfigurations.java
+++ Novo-Imperio/src/main/java/br/com/barbershop/util/security/SecurityConfigurations.java
@@ -34,7 +34,6 @@ public class SecurityConfigurations {
                         })
                         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                         .build();
-
     }
 
     @Bean
--- Novo-Imperio/src/main/java/br/com/barbershop/util/security/SecurityFilter.java
+++ Novo-Imperio/src/main/java/br/com/barbershop/util/security/SecurityFilter.java
@@ -43,6 +43,5 @@ public class SecurityFilter extends OncePerRequestFilter {
         }
 
         return null;
-
     }
 }
